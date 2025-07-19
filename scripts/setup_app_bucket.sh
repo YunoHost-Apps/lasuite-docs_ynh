66a15a36424e7e53c6ef64c9bb66f40c0b129971
Florent <florent.git@zeteo.me>
2025-07-19 22:42:19 +0200

    Conform to editorconfig style

--- scripts/setup_app_bucket.sh
+++ scripts/setup_app_bucket.sh
@@ -3,41 +3,41 @@
 set -eEu -o pipefail
 
 if [ "$(whoami)" != "minio" ]; then
-  sudo -u minio "$0" "$@"
-  exit $?
+    sudo -u minio "$0" "$@"
+    exit $?
 fi
 
 mb_opts=()
 help=0
 
 while [ "$#" -gt 0 ]; do
-  case "$1" in
-    --app=*) APP="${1#*=}"; shift 1;;
-    --secret=*) SECRET="${1#*=}"; shift 1;;
-    --with-versioning) mb_opts+=("--with-versioning"); shift 1;;
-    --help*) help=1; shift 1;;
+    case "$1" in
+        --app=*) APP="${1#*=}"; shift 1;;
+        --secret=*) SECRET="${1#*=}"; shift 1;;
+        --with-versioning) mb_opts+=("--with-versioning"); shift 1;;
+        --help*) help=1; shift 1;;
 
-    *) echo "unknown option: $1" >&2; exit 1;;
-  esac
+        *) echo "unknown option: $1" >&2; exit 1;;
+    esac
 done
 
 if [ "$help" == "1" ]; then
-  echo "Usage: $0 --app=\$app --secret=YOUR_SECRET [--with-versioning]"
-  echo ""
-  echo "Set up a MinIO bucket for apps. Meant to be used by packages."
-  echo "Also creates an access key in MinIO who will be granted readwrite access only to this bucket."
-  echo "See the admin documentation for more info: https://github.com/YunoHost-Apps/minio_ynh/blob/master/doc/ADMIN.md"
-  exit 0
+    echo "Usage: $0 --app=\$app --secret=YOUR_SECRET [--with-versioning]"
+    echo ""
+    echo "Set up a MinIO bucket for apps. Meant to be used by packages."
+    echo "Also creates an access key in MinIO who will be granted readwrite access only to this bucket."
+    echo "See the admin documentation for more info: https://github.com/YunoHost-Apps/minio_ynh/blob/master/doc/ADMIN.md"
+    exit 0
 fi
 
 if [ -z "${APP:-}" ]; then
-  echo "Please pass the following mandatory argument: --app=\$app" >&2
-  exit 1
+    echo "Please pass the following mandatory argument: --app=\$app" >&2
+    exit 1
 fi
 
 if [ -z "${SECRET:-}" ]; then
-  echo 'Please pass the following mandatory argument: --secret="YOUR_SECRET"' >&2
-  exit 1
+    echo 'Please pass the following mandatory argument: --secret="YOUR_SECRET"' >&2
+    exit 1
 fi
 
 ACCESS_KEY=${APP//_/-}
@@ -47,41 +47,41 @@ policy_file=$(mktemp --suffix=".$APP.policy.json")
 minio_install_dir=/var/www/minio
 
 cleanup() {
-  rv=$?
-  rm -f "$policy_file"
-  exit $rv
+    rv=$?
+    rm -f "$policy_file"
+    exit $rv
 }
 
 trap "cleanup" EXIT
 
 if "$minio_install_dir/mc" ls --json minio/ | jq -r ".key" | grep -q "^$APP/$"; then
-  echo "Bucket already exist"
+    echo "Bucket already exist"
 else
-  # Create a bucket in S3 for the app
-  "$minio_install_dir/mc" mb "${mb_opts[@]}" "minio/$BUCKET"
+    # Create a bucket in S3 for the app
+    "$minio_install_dir/mc" mb "${mb_opts[@]}" "minio/$BUCKET"
 fi
 
 if "$minio_install_dir/mc" admin accesskey ls --json minio/ | jq -r ".svcaccs[]?.accessKey" | grep "^$ACCESS_KEY$"; then
-  # Remove any existing access key and recreate it
-  "$minio_install_dir/mc" admin accesskey remove minio/ "$ACCESS_KEY"
+    # Remove any existing access key and recreate it
+    "$minio_install_dir/mc" admin accesskey remove minio/ "$ACCESS_KEY"
 fi
 
 # Create a new policy to give only access to the app's bucket
 # The policy_file is temporary and will be cleaned up at the end of the execution
 cat <<EOF > "$policy_file"
 {
-  "Version": "2012-10-17",
-  "Statement": [{
-    "Effect": "Allow",
-    "Action": [ "s3:*" ],
-    "Resource": [
-      "arn:aws:s3:::$BUCKET/*",
-      "arn:aws:s3:::$BUCKET"
-    ]
-  }]
+    "Version": "2012-10-17",
+    "Statement": [{
+        "Effect": "Allow",
+        "Action": [ "s3:*" ],
+        "Resource": [
+        "arn:aws:s3:::$BUCKET/*",
+        "arn:aws:s3:::$BUCKET"
+        ]
+    }]
 }
 EOF
 
 # Create the access for the app to the bucket, with the above policy.
 "$minio_install_dir/mc" admin accesskey create minio/ --access-key "$ACCESS_KEY" --secret-key "$SECRET" --policy "$policy_file" \
-  --name "$APP-ynh" --description "Access key for the $APP package for Yunohost"
+    --name "$APP-ynh" --description "Access key for the $APP package for Yunohost"
